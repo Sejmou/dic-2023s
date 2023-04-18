6c76db97c8e78af1a61c2f17e7f77c9d9dc6ed71
soe-tu <e11809642@student.tuwien.ac.at>
2023-04-18 16:52:45 +0200

    adds bash scripts to execute a specific runner script for a given input file with default options using the devset and the DIC_2_runner.py script

---
+++ script_cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script takes two arguments: a file name and a runner name
+# It runs the runner on the file and outputs the results to a file
+
+# If no arguments are given, it runs the default runner on the default file
+# If one argument is given, it runs the default runner on the given file
+# If two arguments are given, it runs the given runner on the given file
+if [ $# -eq 0 ]; then
+  file="hdfs:///user/dic23_shared/amazon-reviews/full/reviews_devset.json"
+  runner="./src/DIC_2_runner.py"
+elif [ $# -eq 1 ]; then
+  file="$1"
+  runner="./src/DIC_2_runner.py"
+elif [ $# -eq 2 ]; then
+  file="$1"
+  runner="$2"
+else
+  echo "Too many arguments"
+  exit 1
+fi
+
+time python "$runner" --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.4.jar -r hadoop "$file" >output_devset.txt
