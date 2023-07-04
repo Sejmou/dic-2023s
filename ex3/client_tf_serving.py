8dc9cde225e2b810aa9d19d6a9b21aef06d13f40
Sejmou <samo.kolter@gmail.com>
2023-07-05 01:56:02 +0200

    client with proper stat collection, adapt result processing script

--- ex3/client_tf_serving.py
+++ ex3/client_tf_serving.py
@@ -25,9 +25,7 @@ import time
 import datetime
 import pandas as pd
 import numpy as np
-
-MODEL_NAME = "ssd_mobilenet_v2_base64"  # the name under which the model is served on the TF Serving server
-API_ENDPOINT_URL = f"http://localhost:8501/v1/models/{MODEL_NAME}:predict"
+from tqdm import tqdm
 
 try:
     coco_dataset_classes = pd.read_json(
@@ -52,10 +50,6 @@ def get_image_paths(folder):
     ]
 
 
-def load_images_from_folder(folder):
-    return [Image.open(path) for path in get_image_paths(folder)]
-
-
 def encode_image(image):
     """
     Encode a PIL image to base64 string
@@ -63,10 +57,6 @@ def encode_image(image):
     buffered = io.BytesIO()
     image.save(buffered, format="JPEG")
     return base64.b64encode(buffered.getvalue()).decode("utf-8")
-
-
-def encode_images(images):
-    return [encode_image(image) for image in images]
 
 
 def create_api_payload(base64_img: str):
@@ -78,19 +68,27 @@ def write_json_to_file(json: str, path: str):
         file.write(json)
 
 
-def get_prediction(base64_img: str):
+def get_prediction(url: str, base64_img: str):
     """
-    Sends a POST request to the API endpoint with the given base64 image as payload.
-    Processes the JSON response and returns a list of the detected bounding boxes.
+    Sends a POST request to the API endpoint with the given base64 image as payload, outputting results.
+
+    Args:
+        url: the URL of the API endpoint
+        base64_img: a base64-encoded JPEG image
+
+    Returns:
+        boxes: a list of bounding boxes of the detected objects in the image
+        response_time: the time it took for the API to respond to the request
     """
     payload = create_api_payload(base64_img)
     headers = {"Content-Type": "application/json"}
-    response = requests.post(API_ENDPOINT_URL, data=payload, headers=headers)
+    response = requests.post(url, data=payload, headers=headers)
     # reponse JSON is a list of dictionaries, each dictionary contains the predictions for one image
     # as the API only accepts one image per request, the list actually only contains a single dictionary
     predictions = response.json()["predictions"][0]
     boxes = process_prediction(predictions)
-    return boxes
+    response_time = response.elapsed.total_seconds()
+    return boxes, response_time
 
 
 def process_prediction(prediction: dict, min_score: float = 0.0):
@@ -132,9 +130,17 @@ def process_prediction(prediction: dict, min_score: float = 0.0):
     return boxes
 
 
+def get_current_timestamp():
+    now = datetime.datetime.now()
+    timestamp_str = now.strftime(
+        "%Y-%m-%dT%H:%M:%S.%fZ"
+    )  # for easy conversion to Python datetime https://stackoverflow.com/a/10805633/13727176
+    return now, timestamp_str
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Client for an object detection API built with TensorFlow Serving, accepting a base64-encoded image as input. Uploads images from a given input folder to the API and stores the bounding boxes of the detected objects received in the response."
+        description="Client for an object detection API built with TensorFlow Serving, accepting a base64-encoded image as input. Uploads images from a given input folder to the API and stores the detection results + stats."
     )
     parser.add_argument(
         "-i",
@@ -147,19 +153,29 @@ if __name__ == "__main__":
         "-o",
         "--output_dir",
         type=str,
-        help="Path to directory where API response JSON should be stored.",
-        default="data",
+        help="Path to directory where results should be stored.",
+        default=os.path.join("data", "results"),
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        help="Name of the model (API endpoint) to use for inference.",
+        required=True,
     )
 
     args = parser.parse_args()
     input_dir = args.input_dir
     output_dir = args.output_dir
+    model = args.model
+
+    url = f"http://localhost:8501/v1/models/{model}:predict"
 
     if not os.path.isdir(input_dir):
         raise ValueError(f"Input directory '{input_dir}' does not exist.")
 
     if not os.path.isdir(output_dir):
-        raise ValueError(f"Output directory '{output_dir}' does not exist.")
+        os.makedirs(output_dir, exist_ok=True)
 
     img_paths = get_image_paths(input_dir)
 
@@ -169,71 +185,56 @@ if __name__ == "__main__":
         )
 
     print(f"Found {len(img_paths)} images in '{input_dir}'")
-    print("Encoding images as array of Base64 strings")
 
-    encoding_start_time = time.time()
-    encoded_images = encode_images(load_images_from_folder(input_dir))
-    encoding_time = time.time() - encoding_start_time
-    print(f"Encoding took {encoding_time} seconds")
+    start_datetime, start_datetime_str = get_current_timestamp()
 
-    filenames = [os.path.basename(path) for path in img_paths]
+    # warm up the API with 3 dummy requests (experiments showed that first request is always slower)
+    print("Warming up API with 3 dummy requests...")
+    for _ in tqdm(list(range(3))):
+        dummy_base64_img = encode_image(Image.open(img_paths[0]))
+        get_prediction(url, dummy_base64_img)
 
-    boxes = {}
-    for filename, img in zip(filenames, encoded_images):
-        boxes[filename] = get_prediction(img)
+    boxes_per_image = {}
+    response_times = []
+    print(
+        "Processing images (one after another: loading, encoding to Base64, sending to API, storing result)"
+    )
+    with tqdm(total=len(img_paths)) as pbar:
+        for path in img_paths:
+            # Load image with Pillow and encode it as base64
+            pil_img = Image.open(path)
+            base64_img = encode_image(pil_img)
 
-    upload_start_datetime = datetime.datetime.now()
-    timestamp_str = upload_start_datetime.strftime(
-        "%Y-%m-%dT%H:%M:%S.%fZ"
-    )  # for easy conversion to Python datetime https://stackoverflow.com/a/10805633/13727176
+            # Send request to API and store results
+            boxes, response_time = get_prediction(url, base64_img)
 
-    # TODO: figure out how to measure times and other metrics like in client_flask_api.py
-    # response = requests.post(url, data=payload, headers=headers)
-    # response_received_time = time.time()
-    # request_time = (
-    #     response.elapsed.total_seconds()
-    # )  # https://stackoverflow.com/a/43260678/13727176
-    # print(f"Received response with status code {response.status_code}")
-    # print(
-    #     f"Request (sending input data and receiving response with results) took {request_time} seconds"
-    # )
+            filename = os.path.basename(path)
+            boxes_per_image[filename] = boxes
+            response_times.append(response_time)
+            pbar.update(1)
+        end_datetime, end_datetime_str = get_current_timestamp()
 
-    # response_content = response.json()
-
-    # server_processing_time = response_content["processing_time"]
-    # print(f"Server processed data in {server_processing_time} seconds")
-
-    # data_transfer_time = request_time - server_processing_time
-    # print(
-    #     f"Data transfer between server and client (request (client -> server + response (server -> client)) took {data_transfer_time} seconds"
-    # )
-
-    # upload_received_datetime = datetime.datetime.strptime(
-    #     response_content["request_received_at"], "%Y-%m-%dT%H:%M:%S.%fZ"
-    # )
-    # upload_time = (upload_received_datetime - upload_start_datetime).total_seconds()
-    # print(f"Upload (sending request from client to server) took {upload_time} seconds")
+    average_response_time = np.mean(response_times)
 
     result = {}
     result["boxes"] = boxes
-    # result["api_response"] = response_content
-    # result["data_transfer_time"] = data_transfer_time
-    # result["upload_time"] = upload_time
-    # result["server_processing_time"] = server_processing_time
-    # result["total_request_time"] = request_time
-    # result["request_sent_at"] = timestamp_str
-    result["requests_started_at"] = timestamp_str
-    # result["upload_time"] = upload_time
-    result["input_folder_name"] = input_dir.split("/")[-1]
-    result["api_url"] = API_ENDPOINT_URL
+    result["started_at"] = start_datetime_str
+    result["finished_at"] = end_datetime_str
+    result["duration"] = (end_datetime - start_datetime).total_seconds()
+    result["input_folder"] = input_dir.split("/")[-1]
+    result["number_of_images"] = len(img_paths)
+    result["response_times"] = response_times
+    result["average_response_time"] = average_response_time
+    result["api_url"] = url
+    result["model"] = model
+    # TODO: figure out if time spent processing on server can be measured somehow to differentiate between server and network latency
+    # TODO: figure out how to measure time spent uploading each image to server
 
     output_file_path = os.path.join(
         output_dir,
-        f"result_{input_dir.split('/')[-1]}_{timestamp_str}.json",
+        f"{'l' if 'localhost' in url else 'r'}_{input_dir.split('/')[-1]}({model})_{start_datetime_str}.json",
     )
     print(f"Writing result to '{output_file_path}'")
-
-    os.makedirs(output_dir, exist_ok=True)
     write_json_to_file(
         json.dumps(result),
         output_file_path,
--- ex3/process_obj_detection_results.py
+++ ex3/process_obj_detection_results.py
@@ -1,3 +1,7 @@
+"""
+Draws labeled boxes for all detected objects onto each image in the provided 'image_folder_path'.
+Assumes all images were passed to the object detection API.
+"""
 import os
 import numpy as np
 from PIL import Image
@@ -53,22 +57,26 @@ def draw_bounding_box_on_image(
         text_bottom -= text_height - 2 * margin
 
 
-def draw_detected_boxes(image_path: str, boxes: list, min_score=0.1):
+def draw_detected_boxes(image_path: str, boxes: list, min_score: float):
     """
     Draws labeled boxes for all detected objects in the image. Assumption: the image was passed to the object detection API.
 
     Processes the 'boxes' part of the API response that contains an array of detected 'boxes' for the image.
+    Each box is a dict with the following keys: 'ymin', 'xmin', 'ymax', 'xmax', 'class_name', 'score'.
+    The coordinates are expected to be in range [0, 1].
     """
     colors = list(ImageColor.colormap.values())
     img = Image.open(image_path)
+    img_width = img.width
+    img_height = img.height
     font = ImageFont.load_default()
     for box in boxes:
         score = box["score"]
         if score >= min_score:
-            ymin = box["ymin"]
-            xmin = box["xmin"]
-            ymax = box["ymax"]
-            xmax = box["xmax"]
+            ymin = box["ymin"] * img_height
+            xmin = box["xmin"] * img_width
+            ymax = box["ymax"] * img_height
+            xmax = box["xmax"] * img_width
             class_name = box["class_name"]
             score = box["score"]
             display_str = "{}: {}%".format(class_name, int(100 * score))
@@ -86,27 +94,37 @@ def draw_detected_boxes(image_path: str, boxes: list, min_score=0.1):
     return img
 
 
-def store_imgs_with_detected_boxes(image_folder_path, bounding_boxes, min_score=0.1):
+def store_imgs_with_detected_boxes(
+    image_folder_path, bounding_boxes, out_dir, min_score=None
+):
     """
-    Draws labeled boxes for all detected objects onto each image in the provided 'image_folder_path', assuming all images were passed to the object detection API.
-    Processes the 'bounding_boxes' part of the API response that contains an array of objects with the 'filename' and detected 'boxes' for each image.
-
-    Each image is stored in a 'results' subfolder of the provided 'image_folder_path', with the same name.
+    Draws labeled boxes for all objects in the provided image_folder_path using the bounding_boxes dict, assuming all images were passed to the object detection API.
     Output images also include formatted scores and label names for every bounding box.
-    """
 
+    Args:
+        image_folder_path (str): Path to directory where images are stored.
+        bounding_boxes (dict): Dictionary with the 'filename' of each processed images as key and its detected bounding boxes as the value.
+        out_dir (str): Name of the subfolder where the processed images are stored.
+        min_score (float, optional): Minimum confidence score for a bounding box to be drawn. Defaults to 0.1.
+
+    Each processed image is stored in the specified subfolder with its original name.
+    """
     print(
         f"Processing detection results for {len(bounding_boxes)} images in folder '{image_folder_path}'"
     )
-    print(f"Storing images with detected boxes in '{image_folder_path}/results'")
+    print(f"Storing images with detected boxes in '{image_folder_path}/{out_dir}'")
 
-    for image in tqdm(bounding_boxes):
-        # print(f"Processing '{image['filename']}'")
-        image_path = image_folder_path + "/" + image["filename"]
-        boxes = image["boxes"]
-        # print(f"Model detected {len(boxes)} objects in image.")
+    min_score = min_score or 0.1
+
+    filenames_and_boxes = list(bounding_boxes.items())
+
+    for f_and_b in tqdm(filenames_and_boxes):
+        filename, boxes = f_and_b
+        print(f"Processing '{filename}'")
+        image_path = os.path.join(image_folder_path, filename)
+        print(f"Model detected {len(boxes)} objects in image.")
         img = draw_detected_boxes(image_path, boxes, min_score=min_score)
-        img.save(os.path.join(image_folder_path, "results", image["filename"]))
+        img.save(os.path.join(out_dir, filename))
 
 
 def load_json(path):
@@ -116,7 +134,7 @@ def load_json(path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Draws labeled boxes for all detected objects onto each image in the provided 'image_folder_path', assuming all images were passed to the object detection API. Processes the 'bounding_boxes' part of the API response that contains an array of objects with the 'filename' and detected 'boxes' for each image. Each image is stored in a 'results' subfolder of the provided 'image_folder_path', with the same name. Output images also include formatted scores and label names for every bounding box."
+        description="Draws labeled boxes for all detected objects onto each image in the provided 'image_folder_path', assuming all images were passed to the object detection API. Processes the 'bounding_boxes' part of the API response that contains an array of objects with the 'filename' and detected 'boxes' for each image. Each image is stored in a subfolder of the provided 'image_folder_path', with the same name. Output images also include formatted scores and label names for every bounding box."
     )
     parser.add_argument(
         "-i",
@@ -132,9 +150,23 @@ if __name__ == "__main__":
         help="Path to JSON file with result of object detection.",
         required=True,
     )
+    parser.add_argument(
+        "-o",
+        "--out_dir",
+        type=str,
+        help="Name of the subfolder where the processed images are stored.",
+        required=True,
+    )
+    parser.add_argument(
+        "-s",
+        "--min_score",
+        type=float,
+        help="Minimum confidence score for a bounding box to be drawn.",
+    )
     args = parser.parse_args()
     img_dir = args.img_dir
     result_file = args.result_file
+    min_score = args.min_score
 
     if not os.path.exists(img_dir):
         raise ValueError(f"Directory '{img_dir}' does not exist.")
@@ -151,10 +183,13 @@ if __name__ == "__main__":
     if not result_file.endswith(".json"):
         raise ValueError(f"'{result_file}' is not a JSON file.")
 
-    if not os.path.exists(os.path.join(img_dir, "results")):
-        os.mkdir(os.path.join(img_dir, "results"))
+    out_dir = os.path.join(img_dir, args.out_dir)
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
 
     result = load_json(result_file)
-    bounding_boxes = result["api_response"]["bounding_boxes"]
+    bounding_boxes = result["boxes"]
 
-    store_imgs_with_detected_boxes(img_dir, bounding_boxes)
+    store_imgs_with_detected_boxes(
+        img_dir, bounding_boxes, out_dir, min_score=min_score
+    )
